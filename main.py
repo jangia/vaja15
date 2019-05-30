99268601599c336b91a29aebfa0cc94fab8308ec
Jan Giacomelli <jan.giacomelli@algoritmik.si>
2019-05-30 18:12:42 +0200

    Jinja template

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
-
+import datetime
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    some_text = 'Message from handler!'
+    current_year = datetime.datetime.now().year
+    cities = ["Boston", "Ljubljana", "Dunaj", "Beograd"]
+    logged_in = False
+    return render_template('index.html', some_text=some_text, current_year=current_year, cities=cities, logged_in=logged_in)
+
 
 @app.route('/about')
 def about():
